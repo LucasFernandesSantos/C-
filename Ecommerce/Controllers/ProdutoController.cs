b252c3b90301d84bc146d5118068f946797dac35
Lucas Fernandes <lucasfernandes_06@hotmail.com>
2019-10-22 11:08:09 -0300

    Ecommerce - parte 4 - 20-10-19

---
+++ Ecommerce/Controllers/ProdutoController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ecommerce.DAL;
+using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class ProdutoController : Controller
+    {
+
+        //readonly serve para dizer que o objeto so recebera informaçao no construtor ou na criaçao de objeto;
+        private readonly ProdutoDao _produtoDAO;
+        public ProdutoController(ProdutoDao produtoDao)
+        {
+            _produtoDAO = produtoDao;
+        }
+
+        public IActionResult Cadastrar()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Produto p)
+        {
+
+            _produtoDAO.Cadastrar(p);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult Alterar(Produto p )
+        {
+          /*  Produto p = _produtoDAO.BuscarProdutoPorId(Convert.ToInt32(hdnId));
+            p.Nome = txtNome;
+            p.Preco = Convert.ToDouble(txtPreco);
+            p.Descricao = txtDescricao;
+            p.Quantidade = Convert.ToInt32(txtQuantidade);*/
+
+            _produtoDAO.Alterar(p);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Index()
+        {
+            
+            ViewBag.DataHora = DateTime.Now;
+            return View(_produtoDAO.ListarProdutos());
+        }
+        public IActionResult Remover(int id)
+        {
+            //remover produto
+
+            _produtoDAO.RemoverProduto(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Alterar(int id)
+        {
+         
+            return View(_produtoDAO.BuscarProdutoPorId(id));
+        }
+    }
+}
---
+++ Ecommerce/DAL/ProdutoDao.cs
@@ -0,0 +1,45 @@
+﻿using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DAL
+{
+    public class ProdutoDao
+    {
+        private readonly Context ctx;
+        public ProdutoDao(Context context)
+        {
+            ctx = context;
+        }
+
+        public void Cadastrar(Produto p)
+        {
+            ctx.Produtos.Add(p);
+            ctx.SaveChanges();
+        }
+
+        public List<Produto> ListarProdutos()
+        {
+            return ctx.Produtos.ToList();
+        }
+
+        public Produto BuscarProdutoPorId(int id)
+        {
+            return ctx.Produtos.Find(id);
+        }
+        public void RemoverProduto(int id)
+        {
+            ctx.Produtos.Remove(BuscarProdutoPorId(id));
+            ctx.SaveChanges();
+        }
+        public void Alterar(Produto p)
+        {
+            ctx.Produtos.Update(p);
+            ctx.SaveChanges();
+        }
+
+      
+    }
+}
